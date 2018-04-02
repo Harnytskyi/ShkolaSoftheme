ced1df12f5e9e696254948d3c94cb7ff80ddbe17
Harnytskyi <andrij.garnytskiy@gmail.com>
2018-04-02 17:56:40 +0300

    1010

---
+++ CSharpHW/25/HW25/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace HW25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter directory path to zip files: ");
+            string dirZipName = Console.ReadLine();
+            Archivator.ZipFilesInDirectory(dirZipName);
+            Archivator.handle.WaitOne();
+
+            Console.WriteLine("Enter Path to unzip files: ");
+            string dirUnzipName = Console.ReadLine();
+
+            Archivator.GetFileFromAchive(dirUnzipName);
+            Console.ReadLine();
+        }
+    }
+}
