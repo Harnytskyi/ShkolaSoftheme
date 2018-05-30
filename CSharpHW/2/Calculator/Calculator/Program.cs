3bc855b86e08bcb9ffd2bddb369e791391febb9c
Harnytskyi <andrij.garnytskiy@gmail.com>
2018-05-31 00:52:57 +0300

    HW2 added

---
+++ CSharpHW/2/Calculator/Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First operand");
+            double first = double.Parse(Console.ReadLine());
+            Console.WriteLine("Operation");
+            var operation = Console.ReadLine();
+            Console.WriteLine("Second operand");
+            double second = double.Parse(Console.ReadLine());
+            Console.WriteLine("Result");
+            calculate(first, second, operation);
+            Console.ReadKey();
+        }
+        private static void calculate(double first, double second, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine(Math.Round(first + second, 2));
+                    break;
+                case "-":
+                    Console.WriteLine(Math.Round(first - second, 2));
+                    break;
+                case "*":
+                    Console.WriteLine(Math.Round(first * second, 2));
+                    break;
+                case "/":
+                    Console.WriteLine(Math.Round(first / second, 2));
+                    break;
+            }
+        }
+    }
+}
