c9a009496adc45d787b2997906a04626b8f19fe0
Harnytskyi <andrij.garnytskiy@gmail.com>
2018-06-12 08:49:47 +0300

    HW 12

---
+++ CSharpHW/12/HW12/HW12/Authenticator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12
+{
+    class Authenticator : IAuthenticator
+    {
+        List<IUser> Users = new List<IUser>();
+        public bool Authentic(IUser user)
+        {
+            foreach (User existingUser in Users)
+            {
+                if(existingUser.Name == user.Name && existingUser.Email == user.Email && existingUser.Password == user.Password)
+
+                {
+                    Console.WriteLine("Last time logged in: {0}", existingUser.GetLastLogin());
+                    existingUser.SetLastLogin(DateTime.Now);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void AddUser(IUser user)
+        {
+            Users.Add(user);
+            Console.WriteLine("New user created");
+        }
+        public void PrintUsers()
+        {
+            Console.WriteLine("Available users:");
+            foreach (User user in Users)
+            {
+                Console.WriteLine(user.GetFullInfo());
+            }
+        }
+    }
+}
---
+++ CSharpHW/12/HW12/HW12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12
+{
+    class Program
+    {
+        static Authenticator authenticator = new Authenticator();
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Run();
+                authenticator.PrintUsers();
+            }
+        }
+        static void Run()
+        {
+            string nameOrEmail;
+            string password = String.Empty; ;
+            bool nameGiven = true;
+            Console.WriteLine("Input Name or Email:");
+            string nameOrEmailInput = Console.ReadLine();
+            if (nameOrEmailInput == "exit")
+                Environment.Exit(0);
+            else if (nameOrEmailInput.Contains("@"))
+            {
+                nameGiven = false;
+            }
+            nameOrEmail = nameOrEmailInput;
+            Console.WriteLine("Enter password");
+            string passwordInput = Console.ReadLine();
+            if (passwordInput == "exit")
+                Environment.Exit(0);
+            else
+            {
+                password = passwordInput;
+            }
+            var user = new User(nameOrEmail, password, nameGiven);
+            if (authenticator.Authentic(user) == false)
+                authenticator.AddUser(user);
+        }
+    }
+}
