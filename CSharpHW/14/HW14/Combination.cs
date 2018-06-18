c6875ba1001d5aaf78a933926bf01fa0621c60b4
Harnytskyi <andrij.garnytskiy@gmail.com>
2018-06-19 00:17:00 +0300

    HW 14 Indexers

---
+++ CSharpHW/14/HW14/Combination.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14
+{
+    public class Combination
+    {
+        public Number[] Numbers;
+        int position = 0;
+
+        public Combination()
+        {
+           Numbers = new Number[6];
+        }
+
+        public void AddNumber(int num)
+        {
+            var number = new Number(num);
+            Numbers[position] = number;
+            position++;
+        }
+        public void Generate()
+        {
+            var random = new Random();
+            while (position < Numbers.Length)
+            {
+                AddNumber(random.Next(1, 10));
+            }
+        }
+
+        public Number this[int index]
+        {
+            get
+            {
+                return Numbers[index];
+            }
+            set
+            {
+                Numbers[index] = value;
+            }
+        }
+        public string PrintNumbers()
+        {
+            string print = string.Empty;
+            foreach (Number num in Numbers)
+            {
+                if (num != null)
+                {
+                    print += num.Num + " ";
+                }
+            }
+            return print;
+        }
+    }
+}
---
+++ CSharpHW/14/HW14/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14
+{
+    class Program
+    {
+        static Combination WinCombination;
+        static Combination UserCombination;
+        
+
+
+        static void Main(string[] args)
+        {
+            WinCombination = new Combination();
+            WinCombination.Generate();
+            UserCombination = new Combination();
+            for (int i = 1; i <= 6; i++)
+            {
+                Console.WriteLine("Input {0} number from 0 to 9", i);
+                var userNumber = Console.ReadLine();
+                if (int.Parse(userNumber) >= 0 && int.Parse(userNumber) <= 9)
+                {
+                    UserCombination.AddNumber(int.Parse(userNumber));
+                }
+                else
+                {
+                    Console.WriteLine("The entered value is not valid");
+                    i--;
+                }
+            }
+            Console.WriteLine("Your combination: " + UserCombination.PrintNumbers());
+            Console.WriteLine("Win combination:  " + WinCombination.PrintNumbers());
+            Console.WriteLine("You " + HasUserWon());
+            Console.ReadLine();
+
+        }
+        static string HasUserWon()
+        {
+            if (UserCombination.Equals(WinCombination) == true)
+                return "win";
+            else
+                return "lose";
+        }
+    }
+}
