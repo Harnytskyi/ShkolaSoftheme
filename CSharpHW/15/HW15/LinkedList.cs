90d2db8d6689937cc3a3603557c9adfe7e08480f
Harnytskyi <andrij.garnytskiy@gmail.com>
2018-06-19 20:11:03 +0300

    HW15 Generic

---
+++ CSharpHW/15/HW15/LinkedList.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW15
+{
+class LinkedList<T>
+    {
+        private T[] _array;
+        private int _lenght;
+        private const int _AddLenght= 10;
+
+        public LinkedList(params T[] element)
+        {
+            _array = new T[element.Length+ _AddLenght];
+            _lenght = element.Length;
+            for (var i = 0; i < element.Length; i++)
+            {
+                _array[i] = element[i];
+            }
+        }
+
+        public T this[int index]
+        {
+            get =>_array[index]; 
+            set
+            {
+                if (index < _lenght)
+                {
+                    _array[index] = value;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        public void AddElement(params T[] element)
+        {
+            if (_lenght + element.Length > _array.Length)
+            {
+                var nawArray = new T[element.Length + _lenght+ _AddLenght];
+                Array.Copy(_array, nawArray, _array.Length);
+                _array = nawArray;
+                var a = default(int);
+                for (var i = _lenght; i < element.Length + _lenght; i++)
+                {
+                    _array[i] = element[a++];
+                }
+                _lenght += element.Length;
+            }
+            else
+            {
+                var a = default(int);
+                for (var i = _lenght; i < element.Length + _lenght; i++)
+                {
+                    _array[i] = element[a++];
+                }
+                _lenght += element.Length;
+            }
+
+        }
+
+        public void DeleteElement(T obj)
+        {
+            for(var i =0; i<_lenght; i++)
+            {
+                if (obj.Equals(_array[i]))
+                {
+                    for (var t = i; t < _lenght; t++)
+                    {
+                        _array[t - 1] = _array[t];
+                    }
+                    _lenght--;
+                    break;
+                }
+            }
+        }
+
+        public void DeleteElement(int elementNumber)
+        {
+            if (0 < elementNumber && elementNumber < _lenght)
+            {
+                for (var i = elementNumber; i < _lenght; i++)
+                {
+                    _array[i - 1] = _array[i];
+                }
+                _lenght--;
+            }
+        }
+
+
+        public int Lenght() => _lenght;
+
+        public bool Equals(T obj)
+        {
+            var resalt = default(bool);
+            foreach(var item in _array)
+            {
+                if (obj.Equals(item))
+                {
+                    resalt = true;
+                    break;
+                }
+            }
+            return resalt;
+        }
+
+        public T[] ToArray()
+        {
+            if (_array.Length != _lenght)
+            {
+                var newArray = new T[_lenght];
+                Array.Copy(_array, newArray, _lenght);
+                return newArray;
+            }
+            else
+            {
+                return _array;
+            }
+        }
+
+        public void PrintNumbers(params T[] _array)
+        {
+            foreach (var item in _array)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CSharpHW/15/HW15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            
+            var obj = new LinkedList<int>(array);
+
+            Console.Write("Source array: ");
+            obj.PrintNumbers(array);
+            obj.AddElement( 6, 7, 8, 9, 10 );
+
+            Console.Write("Array lenght after added new elements: ");
+            Console.WriteLine(obj.Lenght());
+
+            obj.DeleteElement(6);
+            Console.Write("Array deleted element: ");
+            Console.WriteLine(obj.Lenght());
+
+            Console.Write("Is there a given element in the array? ");
+            Console.WriteLine(obj.Equals(5));
+
+            int[] array2 = obj.ToArray();
+            Console.Write("Result array: ");
+            obj.PrintNumbers(array2);
+
+            Console.ReadKey();
+        }
+        
+    }
+}
